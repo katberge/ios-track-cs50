37af6151ff4341eebfecf24a84de7574a6c2dba1
Kat Berge <kat.n.berge@gmail.com>
2020-11-20 17:17:06 -0600

    adds base for PokemonViewController and connects name and number labels to that controller

--- Pokedex/Pokedex/PokemonViewController.swift
+++ Pokedex/Pokedex/PokemonViewController.swift
@@ -6,4 +6,10 @@
 //  Copyright © 2020 Kat Berge. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+
+class PokemonViewController: UIViewController {
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var numberLabel: UILabel!
+}

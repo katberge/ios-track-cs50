70b15feae1b19a37c511249614de79ecb8dfdd80
Kat Berge <kat.n.berge@gmail.com>
2020-11-21 13:03:58 -0600

    adds starting code for setting up the table view

---
+++ Notes/Notes/Note.swift
@@ -0,0 +1,14 @@
+//
+//  Note.swift
+//  Notes
+//
+//  Created by Kat Berge on 11/21/20.
+//  Copyright © 2020 Kat Berge. All rights reserved.
+//
+
+import Foundation
+
+struct Note {
+    let id: Int
+    var contents: String
+}

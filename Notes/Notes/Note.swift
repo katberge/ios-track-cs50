e21095a450763da9bf33c588ecc6948d6419963e
Kat Berge <kat.n.berge@gmail.com>
2020-11-21 13:33:51 -0600

    adds note manager struct with connect and create functions

--- Notes/Notes/Note.swift
+++ Notes/Notes/Note.swift
@@ -7,8 +7,55 @@
 //
 
 import Foundation
+import SQLite3
 
 struct Note {
     let id: Int
     var contents: String
+}
+
+struct NoteManager {
+    var database: OpaquePointer!
+    
+    mutating func connect() {
+        // don't do this twice
+        if database != nil {
+            return
+        }
+        
+        do {
+            let databaseURL = try FileManager.default.url(for: .userDirectory, in: .userDomainMask, appropriateFor: nil, create: true).appendingPathComponent("notes.sqlite3")
+            
+            // open and make sure it was successful
+            if sqlite3_open(databaseURL.path, &database) == SQLITE_OK {
+                // sqlite3 will automatically create row_id column
+                if sqlite3_exec(database, "CREATE TABLE IF NOT EXISTS notes (contents TEXT)", nil, nil, nil) != SQLITE_OK {
+                    print("Could not create table.")
+                }
+            }
+            else {
+                print("Could not connect.")
+            }
+        }
+        catch let error {
+            print("\(error): Could not create database.")
+        }
+    }
+    
+    mutating func create() -> Int {
+        connect()
+        
+        var statement: OpaquePointer!
+        // give new notes default text of "New note"
+        if sqlite3_prepare_v2(database, "INSERT INTO notes (contents) VALUES ('New note')", -1, &statement, nil) != SQLITE_OK {
+            print("Could not create query.")
+            return -1
+        }
+        if sqlite3_step(statement) != SQLITE_DONE {
+            print("Could not insert note.")
+            return -1
+        }
+        
+        return Int(sqlite3_last_insert_rowid(database))
+    }
 }
